3039a1e9adbd09c47fb6dbf3b45514a3e58773b0
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-23 20:21:20 -0600

    Added Axios and API key and search functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,28 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#city-search-element");
+function refreshWeather(response) {
+  let currentTemperatureElement = document.querySelector(
+    "#weather-app-temp-value"
+  );
+  let temp = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city-name");
 
-  cityElement.innerHTML = searchInput.value;
-  
+  cityElement.innerHTML = response.data.city;
+  currentTemperatureElement.innerHTML = Math.round(temp);
+}
+
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-name");
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Fall Creek");
